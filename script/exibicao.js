c75a9d076062caeff9b8eb8a701c50346063eb30
dougmendes-bit <80355232+dougmendes-bit@users.noreply.github.com>
2021-07-27 13:56:26 -0300

    Add files via upload

--- script/exibicao.js
+++ script/exibicao.js
@@ -50,4 +50,51 @@ document.querySelector('.containerExibicao').addEventListener('dblclick', functi
     localStorage.setItem('pessoas', JSON.stringify(pessoas))
 })
 
+//-abrindo editor de elementos
+
+document.querySelector('.containerExibicao').addEventListener('click', function(evento){
+    if(evento.target.parentElement == document.querySelector('body')) return
+    if(evento.target.parentElement == document.querySelector('main')) return
+
+    //--
+
+    const modal = document.querySelector('.modal')
+    modal.style.display = 'block'
+
+    //-- selecionando elementos
+    const nome = document.querySelector('.modal__input-nome')
+    const cpf = document.querySelector('.modal__input-cpf')
+    const telefone = document.querySelector('.modal__input-telefone')
+    const email = document.querySelector('.modal__input-email')
+
+    //-- exibindo e alterando os dados
+        
+    for(let i = 0; i < pessoas.length; i++){
+        if(`Nome: ${pessoas[i].name}` == evento.target.parentElement.firstChild.textContent){
+            nome.value = pessoas[i].name
+            cpf.value = pessoas[i].cpf
+            telefone.value = pessoas[i].phone
+            email.value = pessoas[i].email
+
+            const botaoConcluir = document.querySelector('.modal__botao')
+
+            botaoConcluir.addEventListener('click', function(){
+                console.log('clicado')
+                pessoas[i].name = nome.value
+                pessoas[i].cpf = cpf.value
+                pessoas[i].phone = telefone.value
+                pessoas[i].email = email.value
+
+                localStorage.setItem('pessoas', JSON.stringify(pessoas))
+                modal.style.display = 'none'
+                location.reload();
+            })
+
+        }
+    }
+
+})
+
+
+
 
